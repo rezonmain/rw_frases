7406dc883b156b5848d423996a76c50c36822aae
rezonmain <adiazgomez101@gmail.com>
2020-11-21 01:23:48 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+import { frases } from "./data.js";
+
+let getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+function decodeString(str) {
+  let p = [];
+  for (let i = 0; i < str.length; i++) {
+    // let x = str.charCodeAt(i) - 13;
+    // let y = String.fromCharCode(x);
+    p.push(String.fromCharCode(str.charCodeAt(i) - 13));
+  }
+  return p.join("");
+}
+
+let burgerElement = document.getElementById("burger");
+burgerElement.onclick = showFrase;
+
+let frasesSpan = document.createElement("span");
+
+function hasEnded(event) {
+  if (event.type === "animationend") {
+    return true;
+  } else return false;
+}
+
+function showFrase() {
+  burgerElement.style.animation = "rise 1s ease-in-out infinite";
+  burgerElement.style.animationIterationCount = 1;
+  burgerElement.style.animationFillMode = "forwards";
+  burgerElement.addEventListener(
+    "animationend",
+    () => {
+      frasesSpan.innerText = decodeString(frases[getRandomInt(frases.length)]);
+      document.getElementById("frase_container").appendChild(frasesSpan);
+    },
+    false
+  );
+}
